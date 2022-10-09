4292fd2632447df287ee8d99f17711ee48c9e855
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-10-09 17:45:03 -0300

    First Version EX

---
+++ script.js
@@ -0,0 +1,26 @@
+/*Write a JavaScript function that reverses a number. For example, if x = 
+32443 then the output should be 34423.
+Save your JavaScript file and reload the page. Make sure you see the correct output. If 
+not, investigate and fix.*/
+
+
+/*Exercise 2: Write a JavaScript function that returns a string in alphabetical order. For 
+example, if x = 'webmaster' then the output should be 'abeemrstw'. Punctuation and 
+numbers aren't passed in the string.
+Save your JavaScript file and reload the page. Make sure you see the correct output. If 
+not, investigate and fix.*/
+
+
+/*Exercise 3: Write a JavaScript function that converts the first letter of every word to 
+uppercase. For example, if x = "prince of persia" then the output should be "Prince Of 
+Persia".
+Save your JavaScript file and reload the page. Make sure you see the correct output. If 
+not, investigate and fix.*/
+
+
+/*Exercise 4: Write a JavaScript function that finds the longest word in a phrase. For 
+example, if x = "Web Development Tutorial", then the output should be 
+"Development".
+Save your JavaScript file and reload the page. Make sure you see the correct output. If 
+not, investigate and fix*/
+
