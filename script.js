43e1ac9415bcf902282a88ac6f8efb0b78c21258
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-10-09 17:47:56 -0300

    Final changes to design

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/*Write a JavaScript function that reverses a number. For example, if x = 32443 then the output should be 34423.
-Save your JavaScript file and reload the page. Make sure you see the correct output. If not, investigate and fix.*/
+/*Write a JavaScript function that reverses a number. For example, if x = 32443 then the output should be 34423. Save your JavaScript file and reload the page. Make sure you see the correct output. If not, investigate and fix.*/
 
 
 /*Exercise 2: Write a JavaScript function that returns a string in alphabetical order. For example, if x = 'webmaster' then the output should be 'abeemrstw'. Punctuation and numbers aren't passed in the string. Save your JavaScript file and reload the page. Make sure you see the correct output. If not, investigate and fix.*/
